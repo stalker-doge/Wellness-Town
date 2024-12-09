c08a0773a40157c73ccd1a8bf7046c6a9dc4fc21
stalker-doge <94372701+stalker-doge@users.noreply.github.com>
2024-12-09 13:37:34 +0000

    Improved Dialogue System Framework

--- Source/UE_Wellness_Town/Dialogue/DialogueSystem.h
+++ Source/UE_Wellness_Town/Dialogue/DialogueSystem.h
@@ -16,6 +16,24 @@ public:
 	// Sets default values for this component's properties
 	UDialogueSystem();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")//what the speaker says
+	FString DialogueText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")//index of the current line of dialogue
+		int32 DialogueIndex;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")//what the player says
+		FString PlayerDialogueText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")//who is speaking
+	FString SpeakerName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")//what the player could say
+	TArray<FString> DialogueOptions;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")//what the speaker says
+	TArray<FString> DialogueSpeakerLines;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
@@ -23,6 +41,33 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Dialogue")//starts the dialogue
+	void StartDialogue();
+
+	UFUNCTION(BlueprintCallable, Category = "Dialogue")//loads the next line of dialogue
+	void NextLine();
+
+	UFUNCTION(BlueprintCallable, Category = "Dialogue")//sets the player dialogue text to the option selected
+	void PlayerDialogue(int32 Option);
+
+	UFUNCTION(BlueprintCallable, Category = "Dialogue")//ends the dialogue
+	void EndDialogue();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Dialogue")//custom event for starting dialogue
+	void OnDialogueStart();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Dialogue")//custom event for ending dialogue
+	void OnDialogueEnd();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Dialogue")//custom event for loading the next line of dialogue
+	void OnNextLine();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Dialogue")//custom event for player dialogue
+	void OnPlayerDialogue();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Dialogue")//custom event for dialogue options
+	void OnDialogueOptions();
 
 		
 };
